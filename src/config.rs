2aa1782e4b40ad1d76af4975e2ae108ba03f602b
Nathaniel Ledford <nate@nateledford.com>
2020-09-20 10:44:05 -0400

    Create, save, and load config file

---
+++ src/config.rs
@@ -0,0 +1,50 @@
+use std::fs;
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
+use std::path::{Path, PathBuf};
+
+use anyhow::Result;
+
+fn build_config_path() -> PathBuf {
+    let config_dir = dirs::config_dir().unwrap();
+
+    let path = Path::new(config_dir.as_path());
+    let path = path.join("rustfm");
+
+    fs::create_dir_all(&path).expect("Path could not be created");
+
+    path.join("config.json")
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Config {
+    pub api_key: String,
+}
+
+impl Config {
+    pub fn new(api_key: String) -> Self {
+        Self {
+            api_key
+        }
+    }
+
+    pub fn check_if_config_exists() -> bool {
+        Path::exists(build_config_path().as_path())
+    }
+
+    pub fn load_config() -> Result<Self> {
+        let file = File::open(build_config_path())?;
+        let reader = BufReader::new(file);
+        let config = serde_json::from_reader(reader)?;
+
+        Ok(config)
+    }
+
+    pub fn save_config(&self) -> Result<()> {
+        let file = File::create(build_config_path())?;
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &self)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+use std::process;
+
 use clap::Clap;
 
 use rustfm::app::Opts;
+use rustfm::config::Config;
+use std::io;
 
-fn main() {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     let opts = Opts::parse();
 
-    println!("Your last.fm username is `{}`", &opts.username)
+    // First check if a config file has been created
+    // If not, prompt the user to create one
+    if !Config::check_if_config_exists() {
+        println!("Config file does not exist. Creating one now...");
+
+        println!("Enter your Last.fm API key: ");
+        let mut api_key = String::new();
+        io::stdin().read_line(&mut api_key).expect("Failed to read api key");
+
+        let config = Config::new(api_key);
+        config.save_config()?;
+
+        process::exit(0);
+    }
+
+    let config = Config::load_config()?;
+
+    println!("Your last.fm username is `{}`", &opts.username);
+
+    Ok(())
 }
