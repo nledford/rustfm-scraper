f0dad35432a4669103b8db0fa11f6b3fc6ab8dc7
Nathaniel Ledford <nate@nateledford.com>
2020-09-20 16:20:04 -0400

    Fetch tracks from last.fm

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::io;
 use std::process;
+use std::sync::Mutex;
 
 use anyhow::Result;
 use clap::Clap;
+use math::round;
 
 use rustfm::app::Opts;
 use rustfm::config::Config;
-use rustfm::models::{User, UserResponse};
+use rustfm::models::{RecentTracksResponse, Track, User, UserResponse};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -29,21 +31,60 @@ async fn main() -> Result<()> {
 
     let config = Config::load_config()?;
 
-    let user = fetch_profile(opts.username, config.api_key).await?;
+    let user = fetch_profile(&opts.username, &config.api_key).await?;
 
     println!("Username: {}", user.name);
     println!("Number of scrobbles: {}", user.play_count());
 
+    println!("\nFetching tracks...");
+    let tracks = fetch_tracks(&user, &config.api_key, 1, 200, 0).await?;
+
+    println!("\nTotal Tracks: {} (Expected {})", &tracks.len(), &user.play_count());
+
+    println!("\nDone!");
+
     Ok(())
 }
 
-async fn fetch_profile(username: String, api_key: String) -> Result<User> {
-    let url = format!("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user={user}&api_key={api_key}&format=json",
-                      user = username,
-                      api_key = api_key);
-    let resp = reqwest::get(&url).await?.text().await?;
-    println!("{:#?}", &resp);
+fn build_request_url(username: &str, api_key: &str, method: &str) -> String {
+    format!("http://ws.audioscrobbler.com/2.0/?method={method}&user={user}&api_key={api_key}&format=json",
+            method = method,
+            user = username,
+            api_key = api_key)
+}
 
-    let user = reqwest::get(&url).await?.json::<UserResponse>().await?;
-    Ok(user.user)
+async fn fetch_profile(username: &str, api_key: &str) -> Result<User> {
+    let url = build_request_url(username, api_key, "user.getinfo");
+    let user_response = reqwest::get(&url).await?.json::<UserResponse>().await?;
+    Ok(user_response.user)
+}
+
+
+async fn fetch_tracks(user: &User, api_key: &str, page: i32, limit: i32, from: i64) -> Result<Vec<Track>> {
+    use rayon::prelude::*;
+
+    let tracks: Mutex<Vec<Track>> = Mutex::new(Vec::new());
+
+    let number_of_pages = round::ceil(user.play_count() as f64 / limit as f64, 0) as i32;
+
+    (page..=number_of_pages).into_par_iter()
+        .for_each(|page| {
+            print!("\rFetching page {} of {}...", page, number_of_pages);
+
+            let url = format!("http://ws.audioscrobbler.com/2.0/?method={method}&user={user}&api_key={api_key}&format=json&page={page}&limit={limit}&from={from}",
+                              method = "user.getRecentTracks",
+                              user = user.name,
+                              api_key = api_key,
+                              page = page,
+                              limit = limit,
+                              from = from);
+
+            let recent_tracks_response: RecentTracksResponse = reqwest::blocking::get(&url).unwrap().json().unwrap();
+            let mut recent_tracks = recent_tracks_response.recent_tracks.tracks;
+
+            let mut db = tracks.lock().map_err(|_| "Failed to acquire MutexGuard").unwrap();
+            db.append(&mut recent_tracks);
+        });
+
+    Ok(tracks.into_inner().unwrap())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct UserResponse {
@@ -33,5 +34,93 @@ impl User {
 
     pub fn play_count(&self) -> i32 {
         self.playcount.parse().unwrap()
+    }
+}
+
+// ############################################################################
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct RecentTracksResponse {
+    #[serde(rename = "recenttracks")]
+    pub recent_tracks: RecentTracks,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct RecentTracks {
+    #[serde(rename = "@attr")]
+    pub attr: Attr,
+    #[serde(rename = "track")]
+    pub tracks: Vec<Track>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Attr {
+    page: String,
+    #[serde(rename = "perPage")]
+    per_page: String,
+    user: String,
+    total: String,
+    #[serde(rename = "totalPages")]
+    total_pages: String,
+}
+
+impl Attr {
+    pub fn page(&self) -> i32 {
+        self.page.parse().unwrap_or(0)
+    }
+
+    pub fn per_page(&self) -> i32 {
+        self.per_page.parse().unwrap_or(0)
+    }
+
+    pub fn total_pages(&self) -> i32 {
+        self.total_pages.parse().unwrap_or(0)
+    }
+
+    pub fn last_page(&self) -> bool {
+        self.page() == self.total_pages()
+    }
+}
+
+// TODO add field when listening to track
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct Track {
+    pub artist: Artist,
+    pub album: Album,
+    // Skip `image`
+    pub streamable: String,
+    pub date: Date,
+    pub name: String,
+    pub mbid: String,
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct Artist {
+    pub mbid: String,
+    #[serde(rename = "#text")]
+    pub text: String,
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct Album {
+    pub mbid: String,
+    #[serde(rename = "#text")]
+    pub text: String,
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub struct Date {
+    uts: String,
+    #[serde(rename = "#text")]
+    text: String,
+}
+
+impl Date {
+    pub fn time_stamp(&self) -> NaiveDateTime {
+
+        let timestamp: i64 = self.uts.parse().unwrap();
+        let dt = NaiveDateTime::from_timestamp(timestamp, 0);
+
+        dt
     }
 }
