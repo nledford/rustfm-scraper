3fe51eb7cf1d8c5408c49931323e4cfb50b88718
Nathaniel Ledford <nate@nateledford.com>
2020-09-23 08:51:22 -0400

    Fix link

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 use crate::models::Track;
 
-/// A collection of [Track](struct.Track.html)s.
+/// A collection of [Track](../models/struct.Track.html)s.
 /// Typically a single page or the flattened collection of all pages from the Last.fm API.
 pub type Tracks = Vec<Track>;
 
