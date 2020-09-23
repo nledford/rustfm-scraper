37d95750704b0fd9c962ba2a0ce14ff250da2ba3
Nathaniel Ledford <nate@nateledford.com>
2020-09-23 08:48:10 -0400

    Add rustdoc

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,8 @@
 use crate::models::Track;
 
+/// A collection of [Track](struct.Track.html)s.
+/// Typically a single page or the flattened collection of all pages from the Last.fm API.
 pub type Tracks = Vec<Track>;
+
+/// A unflattened collection of track pages from the Last.fm API.
 pub type CollectedTracks = Vec<Tracks>;
