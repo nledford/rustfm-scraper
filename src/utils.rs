f8c3f9293c3e6b15514e81d59c5303d59e181762
Nathaniel Ledford <nate@nateledford.com>
2020-09-23 08:41:22 -0400

    Remove unused function

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,3 @@
-use std::time;
-
-use rand::Rng;
-
-pub fn gen_random_duration() -> time::Duration {
-    let mut rng = rand::thread_rng();
-
-    let duration = rng.gen_range(100, 500);
-    time::Duration::from_millis(duration)
-}
 
 pub fn get_current_unix_timestamp() -> i64 {
     chrono::offset::Utc::now().timestamp()
