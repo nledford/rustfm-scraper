3aaf246f1909d934da4c8ce3253888fc51de050f
Nathaniel Ledford <nate@nateledford.com>
2020-09-26 09:56:03 -0400

    Move fetch code to distinct function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 use clap::Clap;
 
 use rustfm_scraper::{config, files, lastfm, utils};
-use rustfm_scraper::app::Opts;
+use rustfm_scraper::app::{Fetch, Opts};
 use rustfm_scraper::app::SubCommand;
 use rustfm_scraper::config::Config;
 
@@ -23,85 +23,91 @@ async fn main() -> Result<()> {
 
     match opts.subcmd {
         SubCommand::Fetch(f) => {
-            let username = match f.username {
-                Some(username) => username,
-                None => config.default_username
-            };
-
-            println!("Fetching user profile `{}`...", &username);
-            let user = lastfm::fetch_profile(&username, &config.api_key).await?;
-
-            println!("\nUsername: {}", user.name);
-            println!("Number of scrobbles: {}", user.play_count());
-
-            let mut append_tracks = false;
-            let mut saved_tracks = if f.append {
-                append_tracks = true;
-                println!("Loading existing tracks from the local hard drive...");
-                files::load_from_csv(&user.name)
-            } else {
-                Vec::new()
-            };
-
-            let min_timestamp = match saved_tracks.get(0) {
-                Some(track) => track.timestamp_utc + 10,
-                None => 0,
-            };
-
-            let page = match f.page {
-                Some(page) => {
-                    if page <= 0 {
-                        1
-                    } else {
-                        page
-                    }
-                }
-                None => 1,
-            };
-
-            let limit = match f.limit {
-                Some(limit) => {
-                    if limit <= 0 {
-                        50
-                    } else if limit > 200 {
-                        200
-                    } else {
-                        limit
-                    }
-                }
-                None => 50,
-            };
-
-            let from = match f.from {
-                Some(from) => from,
-                None => 0,
-            };
-
-            let to = match f.to {
-                Some(to) => to,
-                None => utils::get_current_unix_timestamp(),
-            };
-
-            if append_tracks {
-                let new_tracks =
-                    lastfm::fetch_tracks(&user, &config.api_key, page, limit, min_timestamp, to)
-                        .await?;
-
-                println!(
-                    "Saving {} new tracks to existing file...",
-                    &new_tracks.len()
-                );
-                files::append_to_csv(new_tracks, &mut saved_tracks, &user.name);
-            } else {
-                let tracks =
-                    lastfm::fetch_tracks(&user, &config.api_key, page, limit, from, to).await?;
-
-                println!("Saving {} tracks to file...", &tracks.len());
-                files::save_to_csv(tracks, &user.name);
-            }
-
-            println!("\nDone!");
+            fetch(f, config).await?
         }
+    }
+
+    println!("\nDone!");
+
+    Ok(())
+}
+
+async fn fetch(f: Fetch, config: Config) -> Result<()> {
+    let username = match f.username {
+        Some(username) => username,
+        None => config.default_username
+    };
+
+    println!("Fetching user profile `{}`...", &username);
+    let user = lastfm::fetch_profile(&username, &config.api_key).await?;
+
+    println!("\nUsername: {}", user.name);
+    println!("Number of scrobbles: {}", user.play_count());
+
+    let mut append_tracks = false;
+    let mut saved_tracks = if f.append {
+        append_tracks = true;
+        println!("Loading existing tracks from the local hard drive...");
+        files::load_from_csv(&user.name)
+    } else {
+        Vec::new()
+    };
+
+    let min_timestamp = match saved_tracks.get(0) {
+        Some(track) => track.timestamp_utc + 10,
+        None => 0,
+    };
+
+    let page = match f.page {
+        Some(page) => {
+            if page <= 0 {
+                1
+            } else {
+                page
+            }
+        }
+        None => 1,
+    };
+
+    let limit = match f.limit {
+        Some(limit) => {
+            if limit <= 0 {
+                50
+            } else if limit > 200 {
+                200
+            } else {
+                limit
+            }
+        }
+        None => 50,
+    };
+
+    let from = match f.from {
+        Some(from) => from,
+        None => 0,
+    };
+
+    let to = match f.to {
+        Some(to) => to,
+        None => utils::get_current_unix_timestamp(),
+    };
+
+    if append_tracks {
+        let new_tracks =
+            lastfm::fetch_tracks(&user, &config.api_key, page, limit, min_timestamp, to)
+                .await?;
+
+        println!(
+            "Saving {} new tracks to existing file...",
+            &new_tracks.len()
+        );
+        files::append_to_csv(new_tracks, &mut saved_tracks, &user.name);
+    } else {
+        let tracks =
+            lastfm::fetch_tracks(&user, &config.api_key, page, limit, from, to).await?;
+
+        println!("Saving {} tracks to file...", &tracks.len());
+        files::save_to_csv(tracks, &user.name);
     }
 
     Ok(())
