9adc9dd47f214ce37ea044623e35fc974d8e38ab
Nathaniel Ledford <nate@nateledford.com>
2020-10-07 12:09:27 -0400

    Add generic response for error handling

--- src/lastfm/profile.rs
+++ src/lastfm/profile.rs
@@ -1,5 +1,8 @@
+use std::process;
+
 use anyhow::Result;
 
+use crate::models::ApiResponse;
 use crate::models::user::{User, UserResponse};
 
 pub async fn fetch_profile(username: &str, api_key: &str) -> Result<User> {
@@ -7,6 +10,15 @@ pub async fn fetch_profile(username: &str, api_key: &str) -> Result<User> {
                       method = "user.getInfo",
                       user = username,
                       api_key = api_key);
-    let user_response = reqwest::get(&url).await?.json::<UserResponse>().await?;
-    Ok(user_response.user)
+    let user_response = reqwest::get(&url).await?.json::<ApiResponse<UserResponse>>().await?;
+    let user = match user_response {
+        ApiResponse::Success(user_response) => user_response.user,
+        ApiResponse::Failure(error) => {
+            println!("An error occurred at Last.fm's API:");
+            println!("{}", &error.message);
+            process::exit(0);
+        }
+    };
+
+    Ok(user)
 }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,20 @@
 //! Represents data retrieved from the Last.fm API and stored locally in files
 
+use serde::Deserialize;
+
 pub mod recent_tracks;
 pub mod saved_scrobbles;
 pub mod user;
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum ApiResponse<T> {
+    Success(T),
+    Failure(ErrorResponse),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ErrorResponse {
+    pub error: i32,
+    pub message: String,
+}
