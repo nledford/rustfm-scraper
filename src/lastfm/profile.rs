4b0c2923f9a03f8bc26434dfe42c6ef7ab01a4fc
Nathaniel Ledford <nate@nateledford.com>
2020-10-07 12:11:02 -0400

    Clean up code

--- src/lastfm/profile.rs
+++ src/lastfm/profile.rs
@@ -2,15 +2,19 @@ use std::process;
 
 use anyhow::Result;
 
-use crate::models::ApiResponse;
 use crate::models::user::{User, UserResponse};
+use crate::models::ApiResponse;
 
 pub async fn fetch_profile(username: &str, api_key: &str) -> Result<User> {
     let url = format!("http://ws.audioscrobbler.com/2.0/?method={method}&user={user}&api_key={api_key}&format=json",
                       method = "user.getInfo",
                       user = username,
                       api_key = api_key);
-    let user_response = reqwest::get(&url).await?.json::<ApiResponse<UserResponse>>().await?;
+    let user_response = reqwest::get(&url)
+        .await?
+        .json::<ApiResponse<UserResponse>>()
+        .await?;
+
     let user = match user_response {
         ApiResponse::Success(user_response) => user_response.user,
         ApiResponse::Failure(error) => {
