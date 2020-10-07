9b27867fb5d5e10951a85d54c678600729ee2b4c
Nathaniel Ledford <nate@nateledford.com>
2020-10-07 12:10:29 -0400

    Print errors as error messages

--- src/lastfm/profile.rs
+++ src/lastfm/profile.rs
@@ -14,8 +14,8 @@ pub async fn fetch_profile(username: &str, api_key: &str) -> Result<User> {
     let user = match user_response {
         ApiResponse::Success(user_response) => user_response.user,
         ApiResponse::Failure(error) => {
-            println!("An error occurred at Last.fm's API:");
-            println!("{}", &error.message);
+            eprintln!("An error occurred at Last.fm's API:");
+            eprintln!("{}", &error.message);
             process::exit(0);
         }
     };
