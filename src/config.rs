519657ad04af0991e7a3425c43c3483ea0e65c1a
Nathaniel Ledford <nate@nateledford.com>
2021-06-03 07:34:46 -0400

    Add config option to set default storage format

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,32 @@
+use std::{fs, io};
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::{Path, PathBuf};
-use std::{fs, io};
 
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
 static CRATE_NAME: &str = env!("CARGO_CRATE_NAME");
+
+#[derive(Deserialize, Serialize)]
+pub enum StorageFormat {
+    CSV,
+    JSON,
+    Sqlite,
+}
 
 pub fn initialize_config() -> Result<()> {
     println!("Config file does not exist. Creating one now...");
 
     let api_key = set_api_key();
     let username = set_username();
+    let storage_format = set_storage_format();
 
-    let config = Config::new(api_key, username);
+    let config = Config::new(
+        api_key,
+        username,
+        storage_format,
+    );
 
     Ok(config.save_config()?)
 }
@@ -41,6 +54,40 @@ fn set_username() -> String {
     username.trim().to_string()
 }
 
+fn set_storage_format() -> StorageFormat {
+    let mut valid_selection = false;
+    let mut selection = String::new();
+    let mut storage_format = Some(StorageFormat::CSV);
+
+    while !valid_selection {
+        println!("Select how you would like to save your data: ");
+        println!("1. CSV file");
+        println!("2. JSON file");
+        println!("3. Sqlite database");
+
+        io::stdin()
+            .read_line(&mut selection)
+            .expect("Failed to read selection");
+
+        storage_format = if selection.as_str() == "1" {
+            valid_selection = true;
+            Some(StorageFormat::CSV)
+        } else if selection.as_str() == "2" {
+            valid_selection = true;
+            Some(StorageFormat::JSON)
+        } else if selection.as_str() == "3" {
+            valid_selection = true;
+            Some(StorageFormat::Sqlite)
+        } else {
+            println!("Invalid format selected. Valid values are 1, 2, or 3. Please try again.");
+            None
+        };
+    }
+
+    // We can safely unwrap here because `storage_format` should have a value
+    storage_format.unwrap()
+}
+
 pub fn check_if_config_exists() -> bool {
     Path::exists(build_config_path().as_path())
 }
@@ -49,20 +96,24 @@ pub fn check_if_config_exists() -> bool {
 pub struct Config {
     pub api_key: String,
     pub default_username: String,
+    pub storage_format: StorageFormat,
 }
 
 impl Config {
-    pub fn new(api_key: String, default_username: String) -> Self {
+    pub fn new(api_key: String,
+               default_username: String,
+               storage_format: StorageFormat,
+    ) -> Self {
         Self {
             api_key,
             default_username,
+            storage_format,
         }
     }
 
     pub fn load_config() -> Result<Self> {
         let file = File::open(build_config_path())?;
         let reader = BufReader::new(file);
-        // let config = serde_json::from_reader(reader)?;
 
         let config = match serde_json::from_reader(reader) {
             Ok(config) => config,
